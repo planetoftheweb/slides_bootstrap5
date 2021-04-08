b5c32efc5f6977a538fbf6fe1bb4690477567716
planetoftheweb <ray@planetoftheweb.com>
2021-04-08 13:14:02 -0400

    modified location of build

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './scss/rayveal-theme.scss'
 import 'animate.css/animate.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './css/twemoji-amazing.css'
-import Menu from './js/slideList.json'
+import Menu from './site/js/slideList.json'
 
 //Highlight.js
 import hljs from 'highlight.js'
