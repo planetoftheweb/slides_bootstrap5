51ec8c53dbdead79c2e416b1272311213d70b9b1
Ray Villalobos <ray@planetoftheweb.com>
2021-04-22 18:13:30 -0700

    changes while recording

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ import hljs from 'highlight.js'
 import 'highlight.js/styles/color-brewer.css'
 import Footer from './footer.html?raw'
 
-
-const markdown = import.meta.glob('./src/slides/*.md?raw');
+const markdown = import.meta.glob('./src/slides/*.md?raw')
 
 hljs.highlightAll()
 
@@ -19,10 +18,7 @@ import WebFont from 'webfontloader'
 
 WebFont.load({
   google: {
-    families: [
-      'Source Code Pro:400',
-      'Source Sans Pro:200,300,400,500,700,800,900'
-    ]
+    families: ['Source Code Pro:400', 'Source Sans Pro:200,300,400,500,700,800,900']
   }
 })
 
@@ -30,9 +26,7 @@ WebFont.load({
 function getUrlVars() {
   let vars = [],
     hash
-  let hashes = window.location.href
-    .slice(window.location.href.indexOf('?') + 1)
-    .split('&')
+  let hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&')
 
   for (let i = 0; i < hashes.length; i++) {
     hash = hashes[i].split('=')
@@ -45,8 +39,7 @@ function getUrlVars() {
 // Load external markdown from URL if it exists
 let dataFile = getUrlVars().d
 if (getUrlVars().d) {
-  document.querySelector('section').dataset.markdown =
-    'slides/' + dataFile + '.md'
+  document.querySelector('section').dataset.markdown = 'slides/' + dataFile + '.md'
 } else {
   dataFile = 'index'
 }
@@ -58,7 +51,7 @@ import Notes from 'reveal.js/plugin/notes/notes.esm'
 
 Reveal.initialize({
   plugins: [Markdown, Highlight, Notes],
-  fragments: false, // Globally turn off fragments
+  fragments: true, // Globally turn off fragments
   footer: false, //show footer menu
   margin: 0,
   minScale: 0,
@@ -74,34 +67,28 @@ Reveal.initialize({
   rollingLinks: false
 })
 
-Reveal.addKeyBinding(
-  { keyCode: 84, key: 'T', description: 'Toggle footer display' },
-  () => {
-    var myNode = document.querySelector('.footer')
-    if (myNode !== null) {
-      myNode.classList.toggle('fadeout')
-    }
+Reveal.addKeyBinding({ keyCode: 84, key: 'T', description: 'Toggle footer display' }, () => {
+  var myNode = document.querySelector('.footer')
+  if (myNode !== null) {
+    myNode.classList.toggle('fadeout')
   }
-)
+})
 
-Reveal.addKeyBinding(
-  { keyCode: 77, key: 'M', description: 'Toggle menu display' },
-  () => {
-    var myNode = document.querySelector('.sidebar-menu')
+Reveal.addKeyBinding({ keyCode: 77, key: 'M', description: 'Toggle menu display' }, () => {
+  var myNode = document.querySelector('.sidebar-menu')
 
-    switch (myNode.className) {
-      case 'sidebar-menu':
-        myNode.classList.add('menuin')
-        break
-      case 'sidebar-menu menuin':
-        myNode.className = 'sidebar-menu menuout'
-        break
-      case 'sidebar-menu menuout':
-        myNode.className = 'sidebar-menu menuin'
-        break
-    }
+  switch (myNode.className) {
+    case 'sidebar-menu':
+      myNode.classList.add('menuin')
+      break
+    case 'sidebar-menu menuin':
+      myNode.className = 'sidebar-menu menuout'
+      break
+    case 'sidebar-menu menuout':
+      myNode.className = 'sidebar-menu menuin'
+      break
   }
-)
+})
 
 // When Reveal.js has finished loading
 Reveal.on('ready', () => {
@@ -114,11 +101,9 @@ Reveal.on('ready', () => {
     // Does the slide use the no-fragment option (see demo)
     let hasFragments = null
 
-      ; (currSlide.dataset.state &&
-        currSlide.dataset.state.includes('no-fragment')) ||
-        !Reveal.getConfig().fragments
-        ? (hasFragments = false)
-        : (hasFragments = true)
+    ;(currSlide.dataset.state && currSlide.dataset.state.includes('no-fragment')) || !Reveal.getConfig().fragments
+      ? (hasFragments = false)
+      : (hasFragments = true)
 
     if (hasFragments) {
       // Control which items to add fragments
@@ -162,11 +147,15 @@ Reveal.on('ready', () => {
   menuNav.appendChild(menuHead)
 
   Menu.sort((a, b) => {
-    var x = a.filename.toLowerCase();
-    var y = b.filename.toLowerCase();
-    if (x < y) { return -1; }
-    if (x > y) { return 1; }
-    return 0;
+    var x = a.filename.toLowerCase()
+    var y = b.filename.toLowerCase()
+    if (x < y) {
+      return -1
+    }
+    if (x > y) {
+      return 1
+    }
+    return 0
     return b.filename - a.filename
   }).map(item => {
     if (item.filename !== 'demo' && item.filename !== 'slides') {
